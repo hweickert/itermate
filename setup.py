41fc90a0c7fea7555013875aa1e425e7d5f538e6
Henry Weickert <henryweickert@gmail.com>
2015-09-23 20:42:08 +0200

    adding meta data for pypi upload

--- setup.py
+++ setup.py
@@ -6,5 +6,9 @@ setuptools.setup(
     name="itermate",
     packages = ["itermate"],
     version="1.0.0",
-    install_requires=[]
+    description="Iterator-tools for functional programming.",
+    author = 'Henry Weickert',
+    author_email = 'henryweickert@gmail.com',
+    url = 'https://github.com/hweickert/itermate',
+    keywords = ['itermate', 'itertools', 'iapply', 'imapchain']
 )
