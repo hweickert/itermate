7827db5fb82a745baa54bb9ccf7b94dd66e7a81a
Henry Weickert <henry.weickert@mackevision.com>
2016-01-18 16:21:09 +0100

    adding iprogress() generator to log/print progress updates

--- itermate/__init__.py
+++ itermate/__init__.py
@@ -35,3 +35,42 @@ def unique(iterable):
             continue
         yield i
         yielded.add(i)
+
+
+class iprogress:
+    """ Adds progress feedback to countable (len) list when iterating. """
+
+    def __init__( self, list, callback=lambda s:sys.stdout.write(s+"\n"), mode="percent" ):
+        self._list = list
+        self._callback_core = callback
+        self._mode = mode
+
+        self._index = 0
+        self._amount = len(self._list)
+        self._callback = self._get_callback()
+
+
+    def next( self ):
+        try:
+            result = self._list[self._index]
+            self._index += 1
+            self._callback()
+        except IndexError:
+            raise StopIteration
+        return result
+
+
+    def _get_callback( self ):
+        if self._mode == "percent":
+            result = lambda: self._callback_core( "{0} %".format(int(float(self._index)/(float(self._amount)/100))) )
+        if self._mode == "percent_integer":
+            result = lambda: self._callback_core( int(float(self._index)/(float(self._amount)/100)) )
+        elif self._mode == "float":
+            result = lambda: self._callback_core( float(self._index)/self._amount )
+        elif self._mode == "count":
+            result = lambda: self._callback_core( "{0}/{1}".format(self._index, self._amount) )
+        return result
+
+
+    def __iter__( self ):
+        return self
